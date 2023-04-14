c1b315b624a9f49f924e00e39b967777ac2b70a6
yunjia01 <1120856210@qq.com>
2023-04-14 19:06:06 +0800

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include"day414.h"
+int main()
+{
+	int a = 0; scanf("%d",&a);
+	printf("%d",a);
+	int k = 9, b = 89;
+	int ch=max(k,b);
+	printf("%d",ch);
+	return 0;
+}
+
